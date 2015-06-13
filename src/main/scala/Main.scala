da75fcea235a4c9de245d7b3b5fb6428e8db6923
Edward A Maxedon, Sr <edadma@gmail.com>
2015-06-13 18:48:08 -0400

    add to example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,5 +6,14 @@ object Main extends App
 	val montreal = Geo(45.536482, -73.592702)
 	val phoenix = Geo(33.4500, -112.0667)
 	
+	val m = fromGeo( montreal )
+	val axis = normalize( m cross North )
+	val d100 = angle(100)
+	val n100 = rotate( m, axis, d100 )
+	val sideways = normalize( axis cross m )
+	val e100 = rotate( m, sideways, d100 )
+	
 	println( distance(montreal, phoenix) )
+	println( montreal )
+	println( toGeo(e100), distance(m, e100) )
 }
